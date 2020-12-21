9a0467c93bb4a385573d20deb802572a27799862
Loukek <benotech1@hotmail.com>
2020-12-21 01:36:00 +0100

    Zapisywanie pobranych wieków postaci w tablicy

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -25,24 +25,25 @@ public class Persona {
 
         System.out.println(charList);
 
+        ArrayList<String> ageList = new ArrayList<String>();
+
         for(int i = 0; i < charList.size(); i++) {
             String age = charList.get(i);
             age = age.replaceAll(" ", "_");
             if (age.equals("Protagonist")) {
-                System.out.println(age);
                 Document doc2 = null;
                 try {
                     doc2 = Jsoup.connect("https://megamitensei.fandom.com/wiki/Protagonist_(Persona_5)").get();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
                 Elements ageData = doc2.getElementsByAttributeValue("data-source", "age");
-                System.out.println(ageData.first().getElementsByIndexEquals(1).text());
+                String age1 = ageData.first().getElementsByIndexEquals(1).text();
+                ageList.add(age1);
             } else if (age.equals("Morgana")) {
-                System.out.println(age);
-                System.out.println("Unknown");
+                ageList.add("Unknown");
             } else {
-                System.out.println(age);
                 Document doc2 = null;
                 try {
                     doc2 = Jsoup.connect("https://megamitensei.fandom.com/wiki/" + age).get();
@@ -50,8 +51,17 @@ public class Persona {
                     e.printStackTrace();
                 }
                 Elements ageData = doc2.getElementsByAttributeValue("data-source", "age");
-                System.out.println(ageData.first().getElementsByIndexEquals(1).text());
+                String age1 = ageData.first().getElementsByIndexEquals(1).text();
+                age1 = age1.replace(" (P5) 16 ", "");
+                age1 = age1.replace(" (P5) 17 ", "");
+                age1 = age1.replace(" (P5) 18 ", "");
+                age1 = age1.replace(" (P5) 19 ", "");
+                age1 = age1.replace("(P5S) ", "");
+                ageList.add(age1);
             }
         }
+        System.out.println(ageList);
+
+
     }
 }
